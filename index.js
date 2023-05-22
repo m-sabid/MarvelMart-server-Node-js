2e99c8606fd9c70f2fae03670ef966e70159b797
m-sabid <sabidofficial@gmail.com>
2023-05-22 23:51:48 +0600

    Change local port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 const mongoURI = process.env.MONGODB_URI;
 
 // Enable CORS
