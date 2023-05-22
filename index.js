9bda42b7eb1235ca969e9365cfe38e59688dd5e8
m-sabid <sabidofficial@gmail.com>
2023-05-22 23:14:57 +0600

    Added cors

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 const mongoURI = process.env.MONGODB_URI;
 
+// Enable CORS
+app.use(cors());
+
 // Body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
